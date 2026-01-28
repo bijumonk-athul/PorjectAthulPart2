33428c50e5c38db877c3717b6989cf7378ebc785
bijumonk-athul <bijumonk@sheridancollege.ca>
2026-01-27 20:31:23 -0500

    featurepart2 changes

---
+++ ProjectAthulPart2/src/main/java/com/mycompany/projectathulpart2/ProjectAthulPart2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectathulpart2;
+
+/**
+ *
+ * @author msi
+ */
+public class ProjectAthulPart2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public double add(double a, double b){
+        return a+b;
+    }
+}
